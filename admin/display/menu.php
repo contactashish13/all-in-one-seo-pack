649e67b4cb1ef510180fc8a8d75c30517295b296
contactashish13 <contactashish13@gmail.com>
2017-11-05 15:43:25 +0530

    PHP Warning: file_exists(): open_basedir restriction in effect #1332

--- admin/display/menu.php
+++ admin/display/menu.php
@@ -24,14 +24,16 @@ class AIOSEOPAdminMenus {
 	 * @since 2.3.11.5
 	 */
 	function add_pro_submenu() {
-		global $submenu;
-		$url = 'https://semperplugins.com/all-in-one-seo-pack-pro-version/?loc=aio_menu';
-		$upgrade_text = __('Upgrade to Pro', 'all-in-one-seo-pack');
-		$submenu['all-in-one-seo-pack/aioseop_class.php'][] = array(
-			"<span class='upgrade_menu_link'>$upgrade_text</span>",
-			'manage_options',
-			$url,
-		);
+		if ( empty( ini_get( 'open_basedir' ) ) ) {
+			global $submenu;
+			$url = 'https://semperplugins.com/all-in-one-seo-pack-pro-version/?loc=aio_menu';
+			$upgrade_text = __('Upgrade to Pro', 'all-in-one-seo-pack');
+			$submenu['all-in-one-seo-pack/aioseop_class.php'][] = array(
+				"<span class='upgrade_menu_link'>$upgrade_text</span>",
+				'manage_options',
+				$url,
+			);
+		}
 	}
 }
 
