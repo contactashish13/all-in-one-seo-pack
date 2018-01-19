93cbbfdd0ce31e253a0904b654b7008757122f08
contactashish13 <contactashish13@gmail.com>
2018-01-19 17:26:09 +0530

    test case

--- tests/test-sitemap.php
+++ tests/test-sitemap.php
@@ -106,6 +106,46 @@ class Test_Sitemap extends AIOSEOP_Unit_Test_Base {
 			)
 		);
 	}
+
+	public function test_schemeless_images() {
+		$posts = $this->setup_posts( 2 );
+
+		$id1 = $this->factory->post->create( array( 'post_type' => 'post', 'post_content' => 'content <img src="http://example.org/image1.jpg">', 'post_title' => 'title with image' ) );
+		$id2 = $this->factory->post->create( array( 'post_type' => 'post', 'post_content' => 'content <img src="//example.org/image2.jpg">', 'post_title' => 'title with image' ) );
+		$id3 = $this->factory->post->create( array( 'post_type' => 'post', 'post_content' => 'content <img src="/image3.jpg">', 'post_title' => 'title with image' ) );
+		$posts['with'] = array( get_permalink( $id1 ), get_permalink( $id2 ), get_permalink( $id3 ) );
+
+		$custom_options = array();
+		$custom_options['aiosp_sitemap_indexes'] = '';
+		$custom_options['aiosp_sitemap_images'] = '';
+		$custom_options['aiosp_sitemap_gzipped'] = '';
+		$custom_options['aiosp_sitemap_posttypes'] = array( 'post' );
+
+		$this->_setup_options( 'sitemap', $custom_options );
+
+		$with = $posts['with'];
+		$without = $posts['without'];
+		$this->validate_sitemap(
+			array(
+					$with[0] => array(
+						'image'	=> true,
+					),
+					$with[1] => array(
+						'image'	=> true,
+					),
+					$with[2] => array(
+						'image'	=> true,
+					),
+					$without[0] => array(
+						'image'	=> false,
+					),
+					$without[1] => array(
+						'image'	=> false,
+					),
+			)
+		);
+	}
+
 }
 
 
