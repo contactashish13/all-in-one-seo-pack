227afab33efe3260e66e1fcb733c699a5a8e8e7e
contactashish13 <contactashish13@gmail.com>
2018-01-29 17:20:12 +0530

    empty

--- tests/base/class-aioseop-test-base.php
+++ tests/base/class-aioseop-test-base.php
@@ -154,7 +154,6 @@ class AIOSEOP_Test_Base extends WP_UnitTestCase {
 	 * An empty test is required otherwise tests won't run.
 	 */
 	public final function test_dont_remove_this_method(){
-		$this->assertTrue(true);
 	}
 
 }
