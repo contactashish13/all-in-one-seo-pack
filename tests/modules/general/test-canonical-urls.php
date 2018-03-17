8f35f62d6553c8522e02f4415ef67385bbf581f3
contactashish13 <contactashish13@gmail.com>
2018-03-17 15:33:56 +0530

    support for php 5.2 and below

--- tests/modules/general/test-canonical-urls.php
+++ tests/modules/general/test-canonical-urls.php
@@ -32,17 +32,18 @@ class Test_Canonical_Urls extends AIOSEOP_Test_Base {
 		$this->assertEquals( 1, $aioseop_options['aiosp_customize_canonical_links'] );
 
 		// now let's check if the filter works.
-		add_filter( 'aiosp_canonical_urls', function( $behavior, $default ) {
-			$behavior['aiosp_can'] = 0;
-			$behavior['aiosp_no_paged_canonical_links'] = 1;
-			$behavior['aiosp_customize_canonical_links'] = 0;
-			return $behavior;
-		}, 10, 2 );
+		add_filter( 'aiosp_canonical_urls', array( $this, 'filter_aiosp_canonical_urls' ), 10, 2 );
 
 		do_action( 'admin_init' );
 		$this->assertEquals( 0, $aioseop_options['aiosp_can'] );
 		$this->assertEquals( 1, $aioseop_options['aiosp_no_paged_canonical_links'] );
 		$this->assertEquals( 0, $aioseop_options['aiosp_customize_canonical_links'] );
+	}
 
+	function filter_aiosp_canonical_urls( $behavior, $default ) {
+		$behavior['aiosp_can'] = 0;
+		$behavior['aiosp_no_paged_canonical_links'] = 1;
+		$behavior['aiosp_customize_canonical_links'] = 0;
+		return $behavior;
 	}
 }
