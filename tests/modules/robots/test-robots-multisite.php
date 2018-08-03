dde969ad839cc060ec4c251e741060a0e6afb38f
Ashish Ravi <contactashish13@gmail.com>
2018-08-03 12:00:21 +0530

    test multisite

---
+++ tests/modules/robots/test-robots-multisite.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Class Test_Robots_Multisite
+ *
+ * @package
+ */
+
+require_once AIOSEOP_UNIT_TESTING_DIR . '/tests/modules/robots/test-robots.php';
+
+/**
+ * Robots test case.
+ *
+ * @function get_blog_option
+ */
+class Test_Robots_Multisite extends Test_Robots {
+
+	/**
+	 * Test.
+	 */
+	public function test_test() {
+		$this->assertTrue(false);
+	}
+
+}
