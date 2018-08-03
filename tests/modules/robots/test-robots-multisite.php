df1f0ed8e9bce7a14d3f26b5a188e9ba894b0f2e
Ashish Ravi <contactashish13@gmail.com>
2018-08-03 12:20:24 +0530

    @function does not work

--- tests/modules/robots/test-robots-multisite.php
+++ tests/modules/robots/test-robots-multisite.php
@@ -9,16 +9,17 @@ require_once AIOSEOP_UNIT_TESTING_DIR . '/modules/robots/test-robots.php';
 
 /**
  * Robots test case for multisite.
- *
- * @function get_active_blog_for_user
  */
 class Test_Robots_Multisite extends Test_Robots {
 
 	/**
 	 * Test.
-	 * @function get_active_blog_for_user
 	 */
 	public function test_test() {
+		if ( ! is_multisite() ) {
+			$this->markTestSkipped( 'Only for multi site' );
+		}
+
 		$this->assertTrue(false);
 	}
 
