09faed73010aa41b5c5c0c26c9b2672191f88def
Ashish Ravi <contactashish13@gmail.com>
2018-08-03 12:14:36 +0530

    @function get_active_blog_for_user

--- tests/modules/robots/test-robots-multisite.php
+++ tests/modules/robots/test-robots-multisite.php
@@ -9,6 +9,8 @@ require_once AIOSEOP_UNIT_TESTING_DIR . '/modules/robots/test-robots.php';
 
 /**
  * Robots test case for multisite.
+ *
+ * @function get_active_blog_for_user
  */
 class Test_Robots_Multisite extends Test_Robots {
 
@@ -16,10 +18,6 @@ class Test_Robots_Multisite extends Test_Robots {
 	 * Test.
 	 */
 	public function test_test() {
-		if ( ! is_multisite() ) {
-			$this->markTestSkipped( 'Only for multi site' );
-		}
-
 		$this->assertTrue(false);
 	}
 
