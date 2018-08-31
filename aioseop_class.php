1b2e7e53e3649b68cb03eb22c56adda4cdbd0bb5
Ashish Ravi <contactashish13@gmail.com>
2018-08-31 22:30:55 +0100

    PR comments
    https://github.com/semperfiwebdesign/aioseop-pro/pull/380#issuecomment-417073451

--- aioseop_class.php
+++ aioseop_class.php
@@ -4762,6 +4762,7 @@ EOF;
 	}
 
 	function admin_menu() {
+		$this->check_recently_activated_modules( $_POST );
 		$file      = plugin_basename( __FILE__ );
 		$menu_name = __( 'All in One SEO', 'all-in-one-seo-pack' );
 
@@ -5138,6 +5139,26 @@ EOF;
 			</div>
 		</div>
 		<?php
+	}
+
+ 	private function check_recently_activated_modules( $post ) {
+		global $aioseop_options;
+		$modules	= array();
+		if ( array_key_exists( 'modules', $aioseop_options ) && array_key_exists( 'aiosp_feature_manager_options', $aioseop_options['modules'] ) ) {
+			$modules = array_keys( $aioseop_options['modules']['aiosp_feature_manager_options'] );
+		}
+		if ( $modules ) {
+			foreach ( $modules as $module ) {
+				$name = str_replace( 'aiosp_feature_manager_enable_', '', $module );
+				if ( empty( $aioseop_options['modules']['aiosp_feature_manager_options'][ $module ] ) && ! empty( $post[ $module ] ) ) {
+					// this module was activated.
+					do_action( $this->prefix . 'activate_' . $name );
+				} else if ( ! empty( $aioseop_options['modules']['aiosp_feature_manager_options'][ $module ] ) && ! isset( $post[ $module ] ) ) {
+					// this module was deactivated. This action should be registered NOT in the specific module but elsewhere because that module is not going to be loaded.
+					do_action( $this->prefix . 'deactivate_' . $name );
+				}
+			}
+		}
 	}
 
 }
