52f4cfa5674bc511d30f2b2f16aea09c3bfd72c1
Ashish Ravi <contactashish13@gmail.com>
2018-09-25 14:59:28 +0530

    test case

--- tests/modules/sitemap/test-sitemap.php
+++ tests/modules/sitemap/test-sitemap.php
@@ -243,4 +243,59 @@ class Test_Sitemap extends Sitemap_Test_Base {
 			array( array( 'all', 'post', 'page', 'attachment', 'product' ), 4, 'product' ),
 		);
 	}
+
+	/**
+	 * Attaches images to posts and checks that the image included in the sitemap is the full size image.
+	 *
+	 * @dataProvider fullSizeImageProvider
+	 */
+	public function test_images_are_full_size( $type ) {
+		$url = null;
+
+		$image_to_use	= 'large-square.png';
+
+		switch ( $type ) {
+			case 'featured':
+				$posts	= $this->setup_posts( 0, 1, 'post', $image_to_use );
+				$url	= $posts['with'][0];
+				break;
+			case 'content':
+				$array		= $this->setup_posts( 1 );
+				$url		= $array['without'][0];
+				$attachment_id	= $this->upload_image_and_maybe_attach( str_replace( '\\', '/', AIOSEOP_UNIT_TESTING_DIR . "/resources/images/$image_to_use" ) );
+				$image_url	= wp_get_attachment_url( $attachment_id );
+				wp_update_post( array( 'ID' => $array['ids']['without'][0], 'post_content' => "blah <img src='$image_url'/>" ) );
+				break;
+		}
+
+		$custom_options = array();
+		$custom_options['aiosp_sitemap_indexes'] = '';
+		$custom_options['aiosp_sitemap_images'] = '';
+		$custom_options['aiosp_sitemap_gzipped'] = '';
+		$custom_options['aiosp_sitemap_posttypes'] = array( 'post' );
+
+		$this->_setup_options( 'sitemap', $custom_options );
+
+		$xml	= $this->validate_sitemap(
+			array(
+				$url => array(
+					'image' => true,
+				),
+			)
+		);
+
+		// the sitemap will contain something like large-square-54.png.
+		$this->assertRegExp( '/large\-square(\-\d+)?\.png/', $xml );
+
+	}
+
+	/**
+	 * Provides arguments to test test_images_are_full_size.
+	 */
+	public function fullSizeImageProvider() {
+		return array(
+			array( 'featured' ),
+			array( 'content' ),
+		);
+	}
 }
