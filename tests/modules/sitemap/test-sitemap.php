6b4dca02b78caa5d0479ae9514891c8f7e552afc
Ashish Ravi <contactashish13@gmail.com>
2018-10-04 15:18:05 +0530

    reorganize test cases for sitemap indexes

--- tests/modules/sitemap/test-sitemap.php
+++ tests/modules/sitemap/test-sitemap.php
@@ -427,54 +427,6 @@ class Test_Sitemap extends Sitemap_Test_Base {
 	}
   
 	/**
-	 * Creates different types of posts, enables indexes and pagination and checks if the posts are being paginated correctly without additional/blank sitemaps.
-	 * @requires PHPUnit 5.7
-	 * @dataProvider enabledPostTypes
-	 */
-	public function test_sitemap_index_pagination( $enabled_post_type, $enabled_post_types_count, $cpt ) {
-		if ( is_multisite() ) {
-			$this->markTestSkipped( 'Only for single site' );
-		}
-
-		// choose numbers which are not multiples of each other.
-		$num_posts = 22;
-		$per_xml = 7;
-
-		if ( in_array( 'post', $enabled_post_type ) ) {
-			$this->factory->post->create_many( $num_posts );
-		}
-
-		if ( in_array( 'page', $enabled_post_type ) ) {
-			$this->factory->post->create_many( $num_posts, array( 'post_type' => 'page' ) );
-		}
-
-		if ( in_array( 'attachment', $enabled_post_type ) ) {
-			$this->create_attachments( $num_posts );
-		}
-
-		if ( ! is_null( $cpt ) ) {
-			register_post_type( $cpt );
-			$this->factory->post->create_many( $num_posts, array( 'post_type' => $cpt ) );
-		}
-
-		$custom_options = array();
-		$custom_options['aiosp_sitemap_indexes'] = 'on';
-		$custom_options['aiosp_sitemap_max_posts'] = $per_xml;
-		$custom_options['aiosp_sitemap_images'] = 'on';
-		$custom_options['aiosp_sitemap_gzipped'] = '';
-		$custom_options['aiosp_sitemap_posttypes'] = $enabled_post_type;
-		$custom_options['aiosp_sitemap_taxonomies'] = array();
-
-			$this->_setup_options( 'sitemap', $custom_options );
-
-		// calculate the number of sitemaps expected in the index. The +1 is for the sitemap_addl.xml that includes the home page.
-		$expected = intval( $enabled_post_types_count * ceil( $num_posts / $per_xml ) + 1 );
-		$got = $this->count_sitemap_elements( array( '<sitemap>' ) );
-
-		$this->assertEquals( $expected, $got['<sitemap>'] );
-	}
-
-	/**
 	 * @requires PHPUnit 5.7
 	 * Tests posts with and without images with dependency on jetpack gallery.
 	 *
@@ -613,30 +565,6 @@ class Test_Sitemap extends Sitemap_Test_Base {
 	}
 
 	/**
-	 * @requires PHPUnit 5.7
-	 * Enables indexes and tests that the index and individual sitemaps are all valid according to the schema.
-	 *
-	 * @ticket 1371 Correct tags order according to Sitemap protocol
-	 */
-	public function test_index() {
-		if ( is_multisite() ) {
-			$this->markTestSkipped( 'Only for single site' );
-		}
-
-		$posts = $this->setup_posts( 2, 2 );
-
-		$custom_options = array();
-		$custom_options['aiosp_sitemap_indexes'] = 'on';
-		$custom_options['aiosp_sitemap_images'] = '';
-		$custom_options['aiosp_sitemap_gzipped'] = '';
-		$custom_options['aiosp_sitemap_posttypes'] = array( 'post' );
-
-		$this->_setup_options( 'sitemap', $custom_options );
-
-		$this->validate_sitemap_index( array( 'post' ) );
-	}
-
-	/**
 	 * Returns the urls to be added to the sitemap.
 	 */
 	public function filter_aiosp_sitemap_addl_pages_only() {
@@ -716,20 +644,6 @@ class Test_Sitemap extends Sitemap_Test_Base {
 	public function filter_aioseop_images_allowed_from_hosts( $hosts ) {
 		$hosts[] = 'www.x.com';
 		return $hosts;
-	}
-
-	/**
-	 * Provides posts types to test test_sitemap_index_pagination against.
-	 */
-	public function enabledPostTypes() {
-		return array(
-			array( array( 'post' ), 1, null ),
-			array( array( 'post', 'page' ), 2, null ),
-			array( array( 'product' ), 1, 'product' ),
-			array( array( 'attachment', 'product' ), 2, 'product' ),
-			array( array( 'all', 'post', 'page' ), 2, null ),
-			array( array( 'all', 'post', 'page', 'attachment', 'product' ), 4, 'product' ),
-		);
 	}
 
 	/**
