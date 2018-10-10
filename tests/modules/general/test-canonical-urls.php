60cbed4bbd041505f04160ba64e5325996883ed8
Ashish Ravi <contactashish13@gmail.com>
2018-10-10 10:49:10 +0530

    test failure

--- tests/modules/general/test-canonical-urls.php
+++ tests/modules/general/test-canonical-urls.php
@@ -13,6 +13,10 @@ require_once AIOSEOP_UNIT_TESTING_DIR . '/base/class-aioseop-test-base.php';
 
 class Test_Canonical_Urls extends AIOSEOP_Test_Base {
 
+	public function setUp() {
+		parent::_setUp();
+	}
+
 	/**
 	 * Canonical URLs on all post type (except post) archive pages should include the &post_type parameter.
 	 *
@@ -27,6 +31,10 @@ class Test_Canonical_Urls extends AIOSEOP_Test_Base {
 		$link = get_month_link( get_the_time( 'Y', $id ), get_the_time( 'm', $id ) );
 		$link_page = add_query_arg( 'post_type', 'page', $link );
 		$links = $this->parse_html( $link_page, array( 'link' ) );
+		
+		$names	 = wp_list_pluck( $links, 'rel' );
+		$this->assertContains( 'canonical', $names );
+
 		foreach ( $links as $link ) {
 			if ( 'canonical' === $link['rel'] ) {
 				$this->assertEquals( $link['href'], $link_page );
