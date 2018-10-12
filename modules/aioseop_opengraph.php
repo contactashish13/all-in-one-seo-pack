47490b8f1ecf025cf738d81385d46793c26bc620
Ashish Ravi <contactashish13@gmail.com>
2018-10-12 11:25:56 +0530

    handle all special characters

--- modules/aioseop_opengraph.php
+++ modules/aioseop_opengraph.php
@@ -1424,8 +1424,8 @@ if ( ! class_exists( 'All_in_One_SEO_Pack_Opengraph' ) ) {
 						} else {
 							// For everything else.
 							foreach ( $filtered_value as $f ) {
-								echo '<meta ' . $tags[ $t ]['name'] . '="' . $v . '" ' . $tags[ $t ]['value'] . '="' . htmlspecialchars_decode( $f, ENT_QUOTES ) . '" />' . "\n";
 								// #1363: use esc_attr( $f ) instead of htmlspecialchars_decode( $f, ENT_QUOTES )
+								echo '<meta ' . $tags[ $t ]['name'] . '="' . $v . '" ' . $tags[ $t ]['value'] . '="' . esc_attr( $f ) . '" />' . "\n";
 							}
 						}
 					}
