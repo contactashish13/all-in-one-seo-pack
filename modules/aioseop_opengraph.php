9074c619153f3341c21e382bdf145bb8442ec6f8
Ashish Ravi <contactashish13@gmail.com>
2019-01-08 19:20:14 +0530

    set og:type to that of post type

--- modules/aioseop_opengraph.php
+++ modules/aioseop_opengraph.php
@@ -1257,7 +1257,18 @@ if ( ! class_exists( 'All_in_One_SEO_Pack_Opengraph' ) ) {
 					$description = get_queried_object()->description;
 				}
 				if ( empty( $type ) ) {
-					$type = 'website';
+					# https://github.com/semperfiwebdesign/aioseop-pro/issues/321
+					if ( AIOSEOPPRO && ( is_category() || is_tag() || is_tax() ) ) {
+						$current_post_type = get_post_type();
+						// check if the post type's object type is set.
+						if ( isset( $aioseop_options[ "aiosp_opengraph_{$current_post_type}_fb_object_type" ] ) ) {
+							$type = $aioseop_options[ "aiosp_opengraph_{$current_post_type}_fb_object_type" ];
+						} elseif ( in_array( $current_post_type, array( 'post', 'page' ) ) ) {
+							$type = 'article';
+						}
+					} else {
+						$type = 'website';
+					}
 				}
 			} elseif ( is_home() && ! is_front_page() ) {
 				// This is the blog page but not the homepage.
