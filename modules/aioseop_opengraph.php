6fe3ed8b67c55fc41303299e0d47f94a7c2125d7
Michael Torbert <michaeltorbert@users.noreply.github.com>
2019-02-26 17:53:25 -0500

    revert code in wrong spot

--- modules/aioseop_opengraph.php
+++ modules/aioseop_opengraph.php
@@ -1193,11 +1193,7 @@ if ( ! class_exists( 'All_in_One_SEO_Pack_Opengraph' ) ) {
 						$description = $aiosp->trim_excerpt_without_filters( $aiosp->internationalize( preg_replace( '/\s+/', ' ', $post->post_excerpt ) ), 1000 );
 
 						if ( ! empty( $this->options['aiosp_opengraph_generate_descriptions'] ) ) {
-              if ( ! AIOSEOPPRO || ( AIOSEOPPRO && apply_filters( $this->prefix . 'generate_descriptions_from_content', true, $post ) ) ) {
-							  $description = $aiosp->trim_excerpt_without_filters( $aiosp->internationalize( preg_replace( '/\s+/', ' ', $post->post_content ) ), 1000 );
-						  }
-            }else{
-							$description = $this->trim_text_without_filters_full_length( $this->internationalize( $post->post_excerpt ) );
+							$description = $aiosp->trim_excerpt_without_filters( $aiosp->internationalize( preg_replace( '/\s+/', ' ', $post->post_content ) ), 1000 );
 						}
 					}
 				}
